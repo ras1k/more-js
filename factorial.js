d06d42e355f08ce377889a9bdce1fe0f451d29ea
ras1k <rasikabdullah@gmail.com>
2023-03-05 01:41:38 +0600

    reverse added

--- factorial.js
+++ factorial.js
@@ -1,11 +1,11 @@
 
-// let sum = 0;
-// for (let i=1; i<=7;i++){
-//     console.log(i);
-//     sum += i;
+let sum = 0;
+for (let i=1; i<=7;i++){
+    console.log(i);
+    sum += i;
 
-// }
-// console.log(sum);
+}
+console.log(sum);
 
 function sumOfnumbers(numbers) {
     let sum = 1;
@@ -18,3 +18,16 @@ function sumOfnumbers(numbers) {
 
 const sum = sumOfnumbers(4);
 console.log(sum);
+
+function fact(numbers){
+    let result = 1;
+    for (let i = numbers; i>= 1; i--){
+        result *= i;
+        // console.log(i, result);
+    }
+    return result;
+}
+
+const number = 4;
+const factresult = fact(number);
+console.log('factorial of :', number,'is',factresult);
